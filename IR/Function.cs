30dc0ae60c2a9f394410df40dc6b1bc379e6dfb6
DexinLi <ldx94@hotmail.com>
2018-03-03 20:30:16 +0800

    Add class Function

---
+++ IR/Function.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace IR
+{
+    public class Function
+    {
+        public List<Block> Blocks;
+        public List<Instruction> Instructions;
+
+        public Function()
+        {
+            Blocks = new List<Block>();
+            Instructions = new List<Instruction>();
+        }
+    }
+}
