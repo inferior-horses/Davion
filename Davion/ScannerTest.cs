286cc0027cc4aba83c39f053a7723cd260a82893
lany9 <lany9@uci.edu>
2018-03-11 11:25:40 +0800

    Scanner works well

---
+++ Davion/ScannerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FrontEnd;
+
+namespace Davion
+{
+    public class ScannerTest
+    {
+    	private Scanner scanner_;
+
+    	public ScannerTest(string file_name){
+    		scanner_ = new Scanner(file_name);
+    	}
+
+    	public void PrintToken(){
+    		TokenHelper.Tokens cur_sym;
+            uint counter = 0;
+    		while((cur_sym = scanner_.GetSym()) != TokenHelper.Tokens.kEndofToken){
+                uint line = scanner_.GetFileLine();
+                if (cur_sym == TokenHelper.Tokens.kErrorToken){
+    				char cur_char = scanner_.DebugCurChar();
+		    		Console.WriteLine("detected error in line {0} at char '{1}' ",
+		    			line, cur_char);
+    			}
+
+                string cur_sym_str = "";
+                if (cur_sym == TokenHelper.Tokens.kNumber)
+                {
+                    cur_sym_str = "NUMBER : " + scanner_.Val;
+                }
+                else if (cur_sym == TokenHelper.Tokens.kIdent) {
+                    cur_sym_str = "IDENTIFIER : " + Scanner.identifier_table[scanner_.Id];
+                }
+                else { 
+                    cur_sym_str = scanner_.Token2String(cur_sym);
+                }
+                Console.WriteLine("Token #{0} in line {1} is {2}", counter++, line, cur_sym_str);
+    		}
+    	}
+
+    }
+}
